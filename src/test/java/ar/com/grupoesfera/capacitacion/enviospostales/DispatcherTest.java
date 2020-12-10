42ef0c64f57e427adef0d3c9ae557d89fdc57ffc
Alejandro Santamaria <alejandro.santamaria@mercadolibre.com.co>
2020-12-10 11:45:18 -0500

    test class

--- src/test/java/ar/com/grupoesfera/capacitacion/enviospostales/DispatcherTest.java
+++ src/test/java/ar/com/grupoesfera/capacitacion/enviospostales/DispatcherTest.java
@@ -1,93 +1,83 @@
 package ar.com.grupoesfera.capacitacion.enviospostales;
 
+import org.assertj.core.data.Percentage;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DispatcherTest {
-/*
+
+    Dispatcher dispatcher;
+
+    @Before
+    public void dadoQueTenemosUnDespachador() {
+        CostCalculator costCalculator = new CostCalculator();
+        TransportSelector transportSelector = new TransportSelector();
+        dispatcher = new Dispatcher(costCalculator, transportSelector);
+    }
+
     //<5 paquetes (0 paquetes?)
     @Test
     public void testearCuatroPaquetesYCuatroKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConCuatroPaquetesYCuatroKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnBicicletaConCostoDe50();
-    }
-
-    @Test
-    public void testearTresPaquetesYCuarentaKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConTresPaquetesYCuarentaKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnAutoConCostoDe50();
-    }
-
-    @Test
-    public void testearDosPaquetesYCienKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConCuatroPaquetesYCienKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnCamionetaConCostoDe50();
+        Dispatch dispatch = dadoQueTenemosUnEnvioConCuatroPaquetesYCuatroKilos();
+        cuandoDespachamosElEnvio(dispatch);
+        entoncesSeEnviaEnConCostoDe(Transport.BICYCLE, 50.0, dispatch);
     }
 
     //<10 paquetes
     @Test
-    public void testearNuevePaquetesYCuatroKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConNuevePaquetesYCuatroKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnBicicletaConCostoDe80();
-    }
-
-    @Test
-    public void testearOchoPaquetesYCuarentaKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConOchoPaquetesYCuarentaKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnAutoConCostoDe80();
-    }
-
-    @Test
-    public void testearOchoPaquetesYCienKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConOchoPaquetesYCienKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnCamionetaConCostoDe80();
+    public void testearOchoPaquetesYOchoKilos() {
+        Dispatch dispatch = dadoQueTenemosUnEnvioConOchoPaquetesYOchoKilos();
+        cuandoDespachamosElEnvio(dispatch);
+        entoncesSeEnviaEnConCostoDe(Transport.CAR, 80.0, dispatch);
     }
 
     //>10 paquetes (+ costo administrativo)
     @Test
-    public void testearOncePaquetesYCuatroKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConOncePaquetesYCuatroKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnBicicletaConCostoDe80();
+    public void testearDieciseisPaquetesYDieciseisKilos() {
+        Dispatch dispatch = dadoQueTenemosUnEnvioConDieciseisPaquetesYDieciseisKilos();
+        cuandoDespachamosElEnvio(dispatch);
+        entoncesSeEnviaEnConCostoDe(Transport.VAN, 187.0, dispatch);
     }
 
-    @Test
-    public void testearDocePaquetesYCuarentaKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConDocePaquetesYCuarentaKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnAutoConCostoDe80();
+    private Dispatch dadoQueTenemosUnEnvioConDieciseisPaquetesYDieciseisKilos() {
+        List<Package> packageList = new LinkedList<>();
+        for(int i = 0; i<16; i++) {
+            packageList.add(new Package(1));
+        }
+        Dispatch dispatch = new Dispatch(packageList, 0.0, "RandomAddress", null);
+        return dispatch;
     }
 
-    @Test
-    public void testearTrecePaquetesYCienKilos() {
-        dadoQueTenemosUnDespachador();
-        dadoQueTenemosUnEnvioConTrecePaquetesYCienKilos();
-        cuandoDespachamosElEnvio();
-        entoncesSeEnviaEnCamionetaConCostoDe80();
+    private void entoncesSeEnviaEnConCostoDe(Transport transport, double cost, Dispatch dispatch) {
+        assertThat(dispatch.getCost()).isCloseTo(cost, Percentage.withPercentage(0.1));
+        assertThat(dispatch.getTransport()).isEqualByComparingTo(transport);
     }
 
-    @Test
-    public void testearUnEnvioDemasiadoPesado() {
+    private void cuandoDespachamosElEnvio(Dispatch dispatch) {
+        dispatcher.sendDispatch(dispatch);
     }
 
-    @Test
-    public void testearUnEnvioSinPaquetes() {
+    private Dispatch dadoQueTenemosUnEnvioConCuatroPaquetesYCuatroKilos() {
+        List<Package> packageList = new LinkedList<>();
+        packageList.add(new Package(1));
+        packageList.add(new Package(1));
+        packageList.add(new Package(1));
+        packageList.add(new Package(1));
+        Dispatch dispatch = new Dispatch(packageList, 0.0, "RandomAddress", null);
+        return dispatch;
     }
 
-*/
-
-    //muy pesado
-
-    //
+    private Dispatch dadoQueTenemosUnEnvioConOchoPaquetesYOchoKilos() {
+        List<Package> packageList = new LinkedList<>();
+        for(int i = 0; i<8; i++) {
+            packageList.add(new Package(1));
+        }
+        Dispatch dispatch = new Dispatch(packageList, 0.0, "RandomAddress", null);
+        return dispatch;
+    }
 }
